d6122c13f5e7d9c0e95f7a14007be86a6fafdf33
ablute-software <nunomarujo@ablute.pt>
2025-06-13 12:31:09 +0100

    Update and rename index.js to index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>ablute_ | Plataforma de Análises</h1>
+      <p>Esta é a versão de teste da aplicação clínica.</p>
+    </div>
+  );
+}
